61b78a6296ba804ccdf86ce9d556844f60508fc1
JJefro <jevjefro@gmail.com>
2022-08-06 16:10:56 +0300

    add networkManager

---
+++ InterestingNumbers/Managers/Network/NetworkManager.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkManager.swift
+//  InterestingNumbers
+//
+//  Created by Jevgenijs Jefrosinins on 06/08/2022.
+//
+
+import Foundation
+
+protocol NetworkManagerProtocol {}
+
+class NetworkManager: NetworkManagerProtocol {}
