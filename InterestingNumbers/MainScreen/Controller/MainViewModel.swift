8110886f600335ace5ff76df3010f52c0c9f6d39
JJefro <jevjefro@gmail.com>
2022-08-16 20:38:15 +0300

    implement network methods to mainViewModel

--- InterestingNumbers/MainScreen/Controller/MainViewModel.swift
+++ InterestingNumbers/MainScreen/Controller/MainViewModel.swift
@@ -9,13 +9,51 @@ import Foundation
 
 protocol MainViewModelProtocol {
     init(network: NetworkManagerProtocol)
+    
+    func fetchData(number: Int)
 }
 
 final class MainViewModel: MainViewModelProtocol {
+
+    enum State {
+        case onReceiveNumberEntity(NumberEntity)
+        case onErrorOccured(Error)
+    }
+
+    public var onStateChanges: ((State) -> Void)?
+    private var currentNumberType: NumberType = .defaultType
 
     private let network: NetworkManagerProtocol
     
     required init(network: NetworkManagerProtocol) {
         self.network = network
+    }
+
+    func update(number: Int, type: NumberType) {
+        currentNumberType = type
+        fetchData(number: number)
+    }
+
+    func fetchData(number: Int) {
+        network.performRequest(by: getURLComponents(by: number).url) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case let .success(entity):
+                print(entity)
+                self.onStateChanges?(.onReceiveNumberEntity(entity))
+            case let .failure(error):
+                self.onStateChanges?(.onErrorOccured(error))
+            }
+        }
+    }
+}
+
+// MARK: - Private Methods
+private extension MainViewModel {
+
+    func getURLComponents(by number: Int) -> URLComponents {
+        var numberURLComponents = Constants.Components.numbersURLComponents
+        numberURLComponents.path = "/\(number)/\(currentNumberType.rawValue)"
+        return numberURLComponents
     }
 }
