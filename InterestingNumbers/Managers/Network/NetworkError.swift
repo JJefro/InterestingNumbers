e6261cc5d45d49406705a0afa767fd1ba25aca6b
JJefro <jevjefro@gmail.com>
2022-08-16 20:43:11 +0300

    add NetworkErrors and NetworkManager

--- InterestingNumbers/Managers/Network/NetworkError.swift
+++ InterestingNumbers/Managers/Network/NetworkError.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+enum NetworkError: Error {
+    case unknownURL
+    case unknownServerResponse
+    case unknownStatusCode
+    case noDataReceived
+}
+
+extension NetworkError: LocalizedError {
+    var localizedDescription: String {
+        switch self {
+        case .unknownURL:
+            return R.string.localizable.networkError_unknownURL()
+        case .unknownServerResponse:
+            return R.string.localizable.networkError_unknownServerResponse()
+        case .unknownStatusCode:
+            return R.string.localizable.networkError_unknownStatusCode()
+        case .noDataReceived:
+            return R.string.localizable.networkError_noDataReceived()
+        }
+    }
+}
--- InterestingNumbers/Managers/Network/NetworkManager.swift
+++ InterestingNumbers/Managers/Network/NetworkManager.swift
@@ -7,6 +7,47 @@
 
 import Foundation
 
-protocol NetworkManagerProtocol {}
+protocol NetworkManagerProtocol {
+    func performRequest(by url: URL?, completion: @escaping (Result<NumberEntity, Error>) -> Void)
+}
 
-class NetworkManager: NetworkManagerProtocol {}
+class NetworkManager: NetworkManagerProtocol {
+
+    func performRequest(by url: URL?, completion: @escaping (Result<NumberEntity, Error>) -> Void) {
+        guard let url = url else {
+            completion(.failure(NetworkError.unknownURL))
+            return
+        }
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                if let error = error { completion(.failure(error)) }
+                guard let response = response as? HTTPURLResponse else {
+                    completion(.failure(NetworkError.unknownServerResponse))
+                    return
+                }
+                guard let data = data else {
+                    completion(.failure(NetworkError.noDataReceived))
+                    return
+                }
+                guard response.statusCode / 100 == 2 else {
+                    completion(.failure(NetworkError.unknownStatusCode))
+                    return
+                }
+                completion(self.parseJSON(data: data))
+            }
+        }
+        task.resume()
+    }
+
+    private func parseJSON(data: Data) -> Result<NumberEntity, Error> {
+        let decoder = JSONDecoder()
+        do {
+            let decoderData = try decoder.decode(NumberModel.self, from: data)
+            return .success(NumberEntity(data: decoderData))
+        } catch {
+            return .failure(error)
+        }
+    }
+}
