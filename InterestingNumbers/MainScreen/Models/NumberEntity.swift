d7239d5e883de0773996d2d20b02d999f1fcf256
JJefro <jevjefro@gmail.com>
2022-08-16 20:39:10 +0300

    add NumberEntity

--- InterestingNumbers/MainScreen/Models/NumberEntity.swift
+++ InterestingNumbers/MainScreen/Models/NumberEntity.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+protocol EntityProtocol: Equatable {
+    init(data: NumberModel)
+}
+
+struct NumberEntity: EntityProtocol {
+    let description: String
+    let number: String
+
+    init(data: NumberModel) {
+        self.description = data.response
+        self.number = String(describing: data.number)
+    }
+}
