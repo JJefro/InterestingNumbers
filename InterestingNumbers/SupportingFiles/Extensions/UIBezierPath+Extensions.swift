e6d0e3fcb837b0265202d4f1324fb9eac5d4665c
JJefro <jevjefro@gmail.com>
2022-08-16 20:36:14 +0300

    add extension to MainViewController. code refactoring

---
+++ InterestingNumbers/SupportingFiles/Extensions/UIBezierPath+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  UIBezierPath+Extensions.swift
+//  InterestingNumbers
+//
+//  Created by Jevgenijs Jefrosinins on 14/08/2022.
+//
+
+import Foundation
---
+++ InterestingNumbers/SupportingFiles/Extensions/UICollectionViewCell+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  UICollectionViewCell+Extensions.swift
+//  InterestingNumbers
+//
+//  Created by Jevgenijs Jefrosinins on 08/08/2022.
+//
+
+import Foundation
