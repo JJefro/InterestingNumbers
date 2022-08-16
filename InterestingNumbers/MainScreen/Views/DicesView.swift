f68585fea2e20324cc8bd90d18ef01a361dfd425
JJefro <jevjefro@gmail.com>
2022-08-16 20:40:10 +0300

    update DicesView / code refactoring

--- InterestingNumbers/MainScreen/Views/DicesView.swift
+++ InterestingNumbers/MainScreen/Views/DicesView.swift
@@ -6,17 +6,25 @@
 //
 
 import Foundation
+import UIKit
 
 class DicesView: UIView {
 
-    private let dicesTintColor: UIColor
-    private let dicesBgColor: UIColor
+    private enum PositionX {
+        case left, center, right
+    }
+
+    private enum PositionY {
+        case top, center, bottom
+    }
+
     private let bgColor: UIColor
     private let cornerRadius: CGFloat = 15
+    
+    public var backgroundDiceNumber = 5
+    public var foregroundDiceNumber = 6
 
-    init(dicesTintColor: UIColor, dicesBgColor: UIColor, backgroundColor: UIColor) {
-        self.dicesTintColor = dicesTintColor
-        self.dicesBgColor = dicesBgColor
+    init(backgroundColor: UIColor) {
         self.bgColor = backgroundColor
         super.init(frame: .zero)
     }
@@ -26,83 +34,101 @@ class DicesView: UIView {
     }
 
     override func draw(_ rect: CGRect) {
-        bgColor.setFill(); UIRectFill(rect)
-        guard let context = UIGraphicsGetCurrentContext() else { return }
-        let dices = [getDiceWithNumberOnePath(), getDiceWithNumberFivePath()]
-        dices.forEach { $0.lineWidth = bounds.width / 30 }
-        context.setFillColor(dicesBgColor.cgColor)
-        context.setStrokeColor(dicesTintColor.cgColor)
-        dices.forEach { $0.fill(); $0.stroke() }
+        bgColor.setFill()
+        UIRectFill(rect)
+
+        let backgroundDiceRect = CGRect(x: rect.midX, y: rect.midY, width: rect.width / 2, height: rect.height / 2)
+        let foregroundDiceRect = CGRect(x: rect.minX, y: rect.minY, width: rect.width / 2, height: rect.height / 2)
+
+        getDicePath(in: backgroundDiceRect, number: backgroundDiceNumber).apply {
+            var transform: CGAffineTransform = .identity
+            transform = transform.translatedBy(x: -rect.width / 60, y: -rect.height / 10)
+            $0.apply(transform)
+            $0.fill(color: R.color.dicesBgColor()!)
+            $0.stroke(color: R.color.dicesTintColor()!)
+        }
+
+        getDicePath(in: foregroundDiceRect, number: foregroundDiceNumber).apply {
+            var transform: CGAffineTransform = .identity
+            transform = transform.translatedBy(x: rect.width / 2.85, y: rect.height / 20)
+            transform = transform.rotated(by: .pi / 4)
+            $0.apply(transform)
+            $0.fill(color: R.color.dicesBgColor()!)
+            $0.stroke(color: R.color.dicesTintColor()!)
+        }
     }
 }
 
-// MARK: - Dices UIBezierPath
+// MARK: - Private Methods
 private extension DicesView {
-    func getDiceWithNumberOnePath() -> UIBezierPath {
-        let dicePath = getRoundedRectanglePath(
-            xPosition: bounds.midX,
-            yPosition: bounds.midY
-        )
-        dicePath.append(getCirclePath(arcCenter: CGPoint(
-            x: bounds.maxX - bounds.midX / 2,
-            y: bounds.maxY - bounds.maxY / 4))
-        )
-        dicePath.apply(CGAffineTransform(
-            translationX: -bounds.width / 60,
-            y: -bounds.width / 10)
-        )
+    func getDicePath(in rect: CGRect, number: Int) -> UIBezierPath {
+        let dicePath = UIBezierPath(roundedRect: rect, cornerRadius: cornerRadius)
+        dicePath.append(getDiceDotPath(in: rect, with: number))
+        dicePath.lineWidth = rect.width / 15
         return dicePath
     }
 
-    func getDiceWithNumberFivePath() -> UIBezierPath {
-        let dicePath = getRoundedRectanglePath(
-            xPosition: bounds.minX,
-            yPosition: bounds.minY
-        )
-        let leftPositionX = bounds.midX / 4
-        let rightPositionX = bounds.midX - bounds.midX / 4
-        let bottomPositionY = bounds.midY - bounds.midY / 4
-        let topPositionY = bounds.midY / 4
-        let centerPosition = CGPoint(x: bounds.midX / 2, y: bounds.midY / 2)
-
-        dicePath.append(getCirclePath(
-            arcCenter: CGPoint(x: leftPositionX, y: topPositionY)))
-        dicePath.append(getCirclePath(
-            arcCenter: CGPoint(x: rightPositionX, y: topPositionY)))
-        dicePath.append(getCirclePath(arcCenter: centerPosition))
-        dicePath.append(getCirclePath(
-            arcCenter: CGPoint(x: leftPositionX, y: bottomPositionY)))
-        dicePath.append(getCirclePath(
-            arcCenter: CGPoint(x: rightPositionX, y: bottomPositionY)))
-
-        dicePath.apply(CGAffineTransform(rotationAngle: .pi / 4))
-        dicePath.apply(CGAffineTransform(
-            translationX: bounds.width / 2.85,
-            y: bounds.height / 12)
-        )
-        return dicePath
+    func getDiceDotPath(in rect: CGRect, with number: Int) -> UIBezierPath {
+        assert(1...6 ~= number, "Dice with such number is not implemented")
+        let dotsPath = UIBezierPath()
+        switch number {
+        case 1:
+            dotsPath.append(getDotPath(in: rect, positionX: .center, positionY: .center))
+        case 2:
+            dotsPath.append(getDotPath(in: rect, positionX: .right, positionY: .top))
+            dotsPath.append(getDotPath(in: rect, positionX: .left, positionY: .bottom))
+        case 3:
+            dotsPath.append(getDotPath(in: rect, positionX: .right, positionY: .top))
+            dotsPath.append(getDotPath(in: rect, positionX: .center, positionY: .center))
+            dotsPath.append(getDotPath(in: rect, positionX: .left, positionY: .bottom))
+        case 4:
+            dotsPath.append(getDotPath(in: rect, positionX: .left, positionY: .top))
+            dotsPath.append(getDotPath(in: rect, positionX: .right, positionY: .top))
+            dotsPath.append(getDotPath(in: rect, positionX: .left, positionY: .bottom))
+            dotsPath.append(getDotPath(in: rect, positionX: .right, positionY: .bottom))
+        case 5:
+            dotsPath.append(getDotPath(in: rect, positionX: .left, positionY: .top))
+            dotsPath.append(getDotPath(in: rect, positionX: .right, positionY: .top))
+            dotsPath.append(getDotPath(in: rect, positionX: .center, positionY: .center))
+            dotsPath.append(getDotPath(in: rect, positionX: .left, positionY: .bottom))
+            dotsPath.append(getDotPath(in: rect, positionX: .right, positionY: .bottom))
+        default:
+            dotsPath.append(getDotPath(in: rect, positionX: .left, positionY: .top))
+            dotsPath.append(getDotPath(in: rect, positionX: .right, positionY: .top))
+            dotsPath.append(getDotPath(in: rect, positionX: .left, positionY: .center))
+            dotsPath.append(getDotPath(in: rect, positionX: .right, positionY: .center))
+            dotsPath.append(getDotPath(in: rect, positionX: .left, positionY: .bottom))
+            dotsPath.append(getDotPath(in: rect, positionX: .right, positionY: .bottom))
+        }
+        return dotsPath
     }
 
-    func getRoundedRectanglePath(xPosition: CGFloat, yPosition: CGFloat) -> UIBezierPath {
-        UIBezierPath(
-            roundedRect: CGRect(
-                x: xPosition,
-                y: yPosition,
-                width: bounds.width / 2,
-                height: bounds.height / 2
-            ),
-            cornerRadius: cornerRadius
-        )
+    private func getDotPath(in rect: CGRect, positionX: PositionX, positionY: PositionY) -> UIBezierPath {
+        var xPosition = CGFloat()
+        var yPosition = CGFloat()
+
+        switch positionX {
+        case .left: xPosition = rect.midX - rect.width / 4
+        case .center: xPosition = rect.midX
+        case .right: xPosition = rect.midX + rect.width / 4
+        }
+
+        switch positionY {
+        case .top: yPosition = rect.midY - rect.height / 4
+        case .center: yPosition = rect.midY
+        case .bottom: yPosition = rect.midY + rect.height / 4
+        }
+
+        return getCirclePath(arcCenter: CGPoint(x: xPosition, y: yPosition), radius: rect.width / 30)
     }
 
-    func getCirclePath(arcCenter: CGPoint) -> UIBezierPath {
+    func getCirclePath(arcCenter: CGPoint, radius: CGFloat) -> UIBezierPath {
         UIBezierPath(
             arcCenter: arcCenter,
-            radius: bounds.width / 60,
+            radius: radius,
             startAngle: 0,
             endAngle: 2 * .pi,
-            clockwise: true
-        )
+            clockwise: true)
     }
 }
 
@@ -115,17 +141,16 @@ import SwiftUI
 struct DicesViewRepresentablePreview: PreviewProvider {
     static var previews: some View {
         ForEach(ColorScheme.allCases, id: \.self, content:
-        Representable(view: DicesView(dicesTintColor: R.color.dicesTintColor()!,
-                                      dicesBgColor: R.color.dicesBgColor()!,
-                                      backgroundColor: R.color.mainViewBG()!
-                                     ))
-        .frame(
-            width: 350,
-            height: 350,
-            alignment: .center
-        )
-        .previewLayout(.sizeThatFits)
-        .preferredColorScheme)
+                    Representable(view: DicesView(
+                        backgroundColor: R.color.mainViewBG()!
+                    ))
+                        .frame(
+                            width: 350,
+                            height: 350,
+                            alignment: .center
+                        )
+                            .previewLayout(.sizeThatFits)
+                            .preferredColorScheme)
     }
 }
 #endif
