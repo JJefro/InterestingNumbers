ed61cc41e6ffc8553d6a67594b72aee3e93de6ea
JJefro <jevjefro@gmail.com>
2022-08-16 20:37:05 +0300

    add extension for mainViewController

--- InterestingNumbers/MainScreen/Controller/MainViewController.swift
+++ InterestingNumbers/MainScreen/Controller/MainViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-final class MainViewController<ContentView: MainContentViewProtocol, ViewModel>: UIViewController where ViewModel: MainViewModelProtocol {
+extension MainViewController where ViewModel == MainViewModel, ContentView == MainContentView {
+    convenience init(network: NetworkManagerProtocol) {
+        self.init(viewModel: MainViewModel(network: network))
+    }
+}
+
+final class MainViewController<ContentView: MainContentViewProtocol, ViewModel: MainViewModelProtocol>: UIViewController {
 
     private let contentView: ContentView
     private let viewModel: ViewModel
 
-    init() {
-        self.viewModel = ViewModel(network: NetworkManager())
+    init(viewModel: ViewModel) {
+        self.viewModel = viewModel
         self.contentView = ContentView()
         super.init(nibName: nil, bundle: nil)
     }
@@ -22,8 +28,13 @@ final class MainViewController<ContentView: MainContentViewProtocol, ViewModel>:
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func loadView() {
         view = contentView
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewModel.fetchData(number: 100)
     }
 }
