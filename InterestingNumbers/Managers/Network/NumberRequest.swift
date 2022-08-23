50398b11d39b0acae178abca5a81542e74582421
JJefro <jevjefro@gmail.com>
2022-08-23 15:27:08 +0300

    add NumberRequest that conforms to DataRequest protocol

--- InterestingNumbers/Managers/Network/NumberRequest.swift
+++ InterestingNumbers/Managers/Network/NumberRequest.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+struct NumberRequest: DataRequest {
+    
+    typealias Model = NumberModel
+    typealias Entity = NumberEntity
+
+    private var number: String
+    private var numberType: NumberType
+
+    var url: URL?
+    var urlComponents: URLComponents { Constants.Components.numbersURLComponents }
+
+    init(number: String, type: NumberType) {
+        self.number = number
+        self.numberType = type
+        self.url = getURL()
+    }
+
+    mutating func getURL() -> URL? {
+        var components = urlComponents
+        components.path = "/\(number)/\(numberType.rawValue)"
+        return components.url
+    }
+}
+
+extension DataRequest where Model == NumberModel, Entity == NumberEntity {
+    func decode(_ data: Data) -> Result<Entity, Error> {
+        let decoder = JSONDecoder()
+        do {
+            let decoderData = try decoder.decode(Model.self, from: data)
+            return .success(Entity(data: decoderData))
+        } catch {
+            return .failure(error)
+        }
+    }
+}
